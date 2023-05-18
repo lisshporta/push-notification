dcf9ef262bd0e79e198de0bbb9c4906ee3332699
lisshporta <lis.shporta@gmail.com>
2023-05-18 15:33:14 +0200

    change default home route

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::group(['middleware' => 'auth'],function(){
     Route::post('/store-token', [NotificationSendController::class, 'updateDeviceToken'])->name('store.token');
